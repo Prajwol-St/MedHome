2c752663d54d11f33d612d67c9e757a5267a81b7
Ishan <ishanjoshi234@gmail.com>
2025-11-30 21:17:15 +0545

    Dashboard Activity

---
+++ app/src/main/java/com/example/medhomeapp/DashboardActivity.kt
@@ -0,0 +1,150 @@
+package com.example.medhomeapp
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.Icon
+import android.media.Image
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.content.MediaType.Companion.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.medhomeapp.ui.theme.Blue10
+import com.example.medhomeapp.ui.theme.Grey
+import com.example.medhomeapp.ui.theme.MedHomeAppTheme
+
+class DashboardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DashboardBody()
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DashboardBody(){
+    val context = LocalContext.current
+    val activity = context as Activity
+
+    data class NavItem(val label: String, val icon: Int)
+    var selectedItem by remember { mutableStateOf(0) }
+
+    var navList = listOf(
+        NavItem("Home", R.drawable.baseline_home_24),
+        NavItem("Records", R.drawable.baseline_receipt_long_24),
+        NavItem("Notifications", R.drawable.baseline_notifications_24),
+        NavItem("Settings", R.drawable.baseline_settings_24),
+    )
+
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Blue10,
+                    titleContentColor = Color.White,
+
+                ),
+                title = {Text("MedHome", style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp
+                ))},
+            )
+        },
+
+        bottomBar = {
+            NavigationBar {
+                navList.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        icon ={
+                            Icon(
+                                painter = painterResource(item.icon),
+                                contentDescription = null
+                            )
+                        },
+                        label = {Text(item.label)},
+                        onClick = {
+                            selectedItem = index
+                        },
+                        selected = selectedItem == index
+
+                    )
+                }
+            }
+        }
+    ){ padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ){
+            when(selectedItem){
+                0->HomeScreen()
+                1->RecordsScreen()
+                2->NotificationScreen()
+                3->SettingsScreen()
+            }
+
+        }
+
+    }
+}
+
+
+
+
+@Preview
+@Composable
+fun PreviewDashboard(){
+    DashboardBody()
+}
