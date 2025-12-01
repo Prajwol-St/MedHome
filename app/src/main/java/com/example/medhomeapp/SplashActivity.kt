3e4b47d69060d305a960c077c02e766b8a3eb78f
Prabhash39 <prabhashsigdel10@gmail.com>
2025-12-01 20:36:45 +0545

    fixed imports and added SplashActivity

---
+++ app/src/main/java/com/example/medhomeapp/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.example.medhomeapp
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.medhomeapp.ui.theme.MedHomeAppTheme
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SplashBody()
+
+        }
+    }
+}
+
+@Composable
+fun SplashBody(){
+
+    val context = LocalContext.current
+    val activity = context as Activity
+}
+
+
+@Preview
+@Composable
+fun SplashPreview(){
+    SplashBody()
+}
