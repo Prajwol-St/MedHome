062d5a20e1f231fc4dcc18576e45850478d4b479
Prabhash39 <prabhashsigdel10@gmail.com>
2025-12-07 23:16:58 +0545

    Created Doctor Model

---
+++ app/src/main/java/com/example/medhomeapp/model/DoctorModel.kt
@@ -0,0 +1,9 @@
+package com.example.medhomeapp.model
+
+data class DoctorModel(
+    val id: Int = 0,
+    val userId: Int = 0,
+    val name: String = "",
+    val specialization: String = "",
+    val type: String = "",
+)
