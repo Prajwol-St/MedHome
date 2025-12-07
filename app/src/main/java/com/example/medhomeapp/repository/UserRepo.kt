45fc6033af3006de4a8fc719e5706a62d32e4ac1
Prabhash39 <prabhashsigdel10@gmail.com>
2025-12-07 23:16:39 +0545

    Created User Repo

--- app/src/main/java/com/example/medhomeapp/repository/UserRepo.kt
+++ app/src/main/java/com/example/medhomeapp/repository/UserRepo.kt
@@ -1,41 +1,57 @@
 package com.example.medhomeapp.repository
 
 import com.example.medhomeapp.model.UserModel
+import javax.security.auth.callback.Callback
 
 interface UserRepo {
-    fun login(email: String, password: String,
-              callback: (Boolean, String?) -> Unit)
 
+    fun login (
+        email: String,
+        password: String,
+        callback: (Boolean, String, UserModel?) -> Unit
+    )
+    fun register(
+        email: String,
+        password: String,
+        callback: (Boolean, String, String?) -> Unit // userId
+    )
 
-    fun signup(user: UserModel.User, password: String,
-               callback: (Boolean, String?) -> Unit)
+    // ADD USER DETAILS TO DATABASE
+    fun addUserToDatabase(
+        userId: String,
+        model: UserModel,
+        callback: (Boolean, String) -> Unit
+    )
 
+    // FORGOT PASSWORD
+    fun forgetPassword(
+        email: String,
+        callback: (Boolean, String) -> Unit
+    )
 
-    fun addUserToDB(user: UserModel.User,
-                    callback: (Boolean, String?) -> Unit)
+    // GET USER BY ID
+    fun getUserById(
+        userId: String,
+        callback: (Boolean, String, UserModel?) -> Unit
+    )
 
+    // GET ALL USERS
+    fun getAllUsers(
+        callback: (Boolean, String, List<UserModel>) -> Unit
+    )
 
-    fun getUserById(userId: String,
-                    callback: (UserModel.User?) -> Unit)
+    // EDIT USER PROFILE
+    fun editProfile(
+        userId: String,
+        model: UserModel,
+        callback: (Boolean, String) -> Unit
+    )
 
-
-    fun getAllUsers(callback: (List<UserModel.User>) -> Unit)
-
-
-
-    fun editProfile(user: UserModel.User,
-                    callback: (Boolean, String?) -> Unit)
-
-
-
-    fun deleteAccount(userId: String, callback:
-        (Boolean, String?) -> Unit)
-
-
-
-    fun resetPassword(email: String,
-                      callback: (Boolean, String?) -> Unit)
-
+    // DELETE USER ACCOUNT
+    fun deleteAccount(
+        userId: String,
+        callback: (Boolean, String) -> Unit
+    )
 
 
 }
