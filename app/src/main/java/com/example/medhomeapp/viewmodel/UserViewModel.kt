c5403c60b8f9ff817051df444db3765ee1e859d3
Prabhash39 <prabhashsigdel10@gmail.com>
2025-12-08 17:40:53 +0545

    Created UserViewModel

---
+++ app/src/main/java/com/example/medhomeapp/viewmodel/UserViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.medhomeapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.medhomeapp.model.UserModel
+import com.example.medhomeapp.repository.UserRepoImpl
+
+class UserViewModel : ViewModel() {
+
+    private val userRepo = UserRepoImpl()
+
+    fun login(email: String, password: String, callback: (Boolean, String, UserModel?) -> Unit) {
+        userRepo.login(email, password, callback)
+    }
+
+    fun register(email: String, password: String, callback: (Boolean, String, String?) -> Unit) {
+        userRepo.register(email, password, callback)
+    }
+
+    fun addUserToDatabase(userId: String, user: UserModel, callback: (Boolean, String) -> Unit) {
+        userRepo.addUserToDatabase(userId, user, callback)
+    }
+
+    fun forgetPassword(email: String, callback: (Boolean, String) -> Unit) {
+        userRepo.forgetPassword(email, callback)
+    }
+
+    fun getUserById(userId: String, callback: (Boolean, String, UserModel?) -> Unit) {
+        userRepo.getUserById(userId, callback)
+    }
+
+    fun getAllUsers(callback: (Boolean, String, List<UserModel>) -> Unit) {
+        userRepo.getAllUsers(callback)
+    }
+
+    fun editUserProfile(userId: String, user: UserModel, callback: (Boolean, String) -> Unit) {
+        userRepo.editProfile(userId, user, callback)
+    }
+
+    fun deleteUser(userId: String, callback: (Boolean, String) -> Unit) {
+        userRepo.deleteAccount(userId, callback)
+    }
+}
