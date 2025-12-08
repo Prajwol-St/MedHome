1df367b58151c217551a8b5b0d70a1866817db56
Prabhash39 <prabhashsigdel10@gmail.com>
2025-12-08 17:39:12 +0545

    Created DoctorModel

--- app/src/main/java/com/example/medhomeapp/model/DoctorModel.kt
+++ app/src/main/java/com/example/medhomeapp/model/DoctorModel.kt
@@ -2,8 +2,20 @@ package com.example.medhomeapp.model
 
 data class DoctorModel(
     val id: Int = 0,
+    val role: String,
     val userId: Int = 0,
     val name: String = "",
     val specialization: String = "",
     val type: String = "",
-)
+){
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "id" to id,
+            "role" to role,
+            "userId" to userId,
+            "name" to name,
+            "specialization" to specialization,
+            "type" to type
+        )
+    }
+}
