d606200dcdd09e22ace78142af83304d585565ff
Prajwol-St <Prajwools272@gmail.com>
2025-12-11 21:38:45 +0545

    next

--- app/src/main/java/com/example/medhomeapp/view/MainActivity.kt
+++ app/src/main/java/com/example/medhomeapp/view/MainActivity.kt
@@ -17,15 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MedHomeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContent {setContent {
+            QrScreen()
+        }
         }
     }
 }
@@ -36,12 +30,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MedHomeAppTheme {
-        Greeting("Android")
-    }
 }
