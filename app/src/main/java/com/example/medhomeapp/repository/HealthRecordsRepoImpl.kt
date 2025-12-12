2c5f4701b36456893774d4163d72511742a73ea4
Ishan <ishanjoshi234@gmail.com>
2025-12-12 14:27:34 +0545

    Added HelathRecordsRepoImpl file

---
+++ app/src/main/java/com/example/medhomeapp/repository/HealthRecordsRepoImpl.kt
@@ -0,0 +1,70 @@
+package com.example.medhomeapp.repository
+
+import android.net.Uri
+import com.example.medhomeapp.model.HealthRecordsModel
+import android.content.Context
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.io.File
+import java.io.FileOutputStream
+
+class HealthRecordsRepoImpl(private val context: Context): HealthRecordsRepo {
+    private val auth = FirebaseAuth.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
+
+    private val userId get() = auth.currentUser?.uid?: ""
+
+    private fun collectionRef() =
+        firestore.collection("users")
+            .document(userId)
+            .collection("health_records")
+
+    //This function saves file locally
+    private fun saveFileLocally(fileUri: Uri): String{
+        val inputStream = context.contentResolver.openInputStream(fileUri)
+        val fileName = "${System.currentTimeMillis()}_${fileUri.lastPathSegment}"
+        val file = File(context.filesDir, fileName)
+
+        FileOutputStream(file).use { output ->
+            inputStream?.copyTo(output)
+        }
+        return file.absolutePath
+    }
+
+    override fun addHealthRecord(
+        record: HealthRecordsModel,
+        fileUri: Uri?,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getHealthRecords(
+        onSuccess: (List<HealthRecordsModel>) -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateHealthRecord(
+        record: HealthRecordsModel,
+        fileUri: Uri?,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteHealthRecord(
+        recordId: String,
+        fileUri: String,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+
+}
