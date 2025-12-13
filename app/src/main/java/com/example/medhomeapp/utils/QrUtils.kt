b3a5d22222b851efb451530190e0065239eb5cee
Prajwol-St <Prajwools272@gmail.com>
2025-12-13 15:13:02 +0545

    try

--- app/src/main/java/com/example/medhomeapp/utils/QrUtils.kt
+++ app/src/main/java/com/example/medhomeapp/utils/QrUtils.kt
@@ -3,27 +3,20 @@ package com.example.medhomeapp.utils
 import android.graphics.Bitmap
 import android.graphics.Color
 import com.google.zxing.BarcodeFormat
-import com.google.zxing.MultiFormatWriter
-import com.google.zxing.common.BitMatrix
+import com.google.zxing.qrcode.QRCodeWriter
+import androidx.core.graphics.set
+import androidx.core.graphics.createBitmap
 
-fun generateQrBitmap(
-    text: String,
-    size: Int = 512
-): Bitmap {
-    val bitMatrix: BitMatrix = MultiFormatWriter().encode(
-        text,
-        BarcodeFormat.QR_CODE,
-        size,
-        size
-    )
+fun generateQrBitmap(text: String, size: Int): Bitmap {
+    val writer = QRCodeWriter()
+    val bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, size, size)
 
-    val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565)
+    val bitmap = createBitmap(size, size)
 
     for (x in 0 until size) {
         for (y in 0 until size) {
-            bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+            bitmap[x, y] = if (bitMatrix[x, y]) Color.BLACK else Color.TRANSPARENT
         }
     }
-
     return bitmap
 }
