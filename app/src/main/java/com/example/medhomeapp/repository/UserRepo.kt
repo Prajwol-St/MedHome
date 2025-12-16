560a11f314f67cbdb587baf57a33047ba47cd7a7
Prabhash39 <prabhashsigdel10@gmail.com>
2025-12-16 21:32:00 +0545

    Modified userRepo to add more functionalities

--- app/src/main/java/com/example/medhomeapp/repository/UserRepo.kt
+++ app/src/main/java/com/example/medhomeapp/repository/UserRepo.kt
@@ -16,42 +16,47 @@ interface UserRepo {
         callback: (Boolean, String, String?) -> Unit // userId
     )
 
-    // ADD USER DETAILS TO DATABASE
+
     fun addUserToDatabase(
         userId: String,
         model: UserModel,
         callback: (Boolean, String) -> Unit
     )
 
-    // FORGOT PASSWORD
+
     fun forgetPassword(
         email: String,
         callback: (Boolean, String) -> Unit
     )
 
-    // GET USER BY ID
+
     fun getUserById(
         userId: String,
         callback: (Boolean, String, UserModel?) -> Unit
     )
 
-    // GET ALL USERS
+
     fun getAllUsers(
         callback: (Boolean, String, List<UserModel>) -> Unit
     )
 
-    // EDIT USER PROFILE
+
     fun editProfile(
         userId: String,
         model: UserModel,
         callback: (Boolean, String) -> Unit
     )
 
-    // DELETE USER ACCOUNT
+
     fun deleteAccount(
         userId: String,
         callback: (Boolean, String) -> Unit
     )
+
+    fun logout()
+
+    fun deleteAuthUser(
+        callback: (Boolean, String) -> Unit)
 
 
 }
