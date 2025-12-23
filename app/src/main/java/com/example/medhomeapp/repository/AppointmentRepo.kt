62b49fb2d4e7d5fa15609741d0ca20688641ce8d
Prajwol-St <Prajwools272@gmail.com>
2025-12-23 14:05:06 +0545

    AppointmentRepo

---
+++ app/src/main/java/com/example/medhomeapp/repository/AppointmentRepo.kt
@@ -0,0 +1,34 @@
+package com.example.medhomeapp.repository
+
+import com.example.medhomeapp.model.AppointmentModel
+
+interface AppointmentRepo {
+    fun addAppointment(
+        appointment: AppointmentModel,
+        callback: (Boolean, String) -> Unit
+    )
+
+    fun updateAppointment(
+        appointmentId: String,
+        appointment: AppointmentModel,
+        callback: (Boolean, String) -> Unit
+    )
+
+    fun deleteAppointment(
+        appointmentId: String,
+        callback: (Boolean, String) -> Unit
+    )
+
+
+    fun getAppointmentsByPatientId(
+        patientId: String,
+        callback: (Boolean, String, List<AppointmentModel>) -> Unit)
+
+
+    fun getAppointmentsByDoctorId(
+        doctorId: String,
+        callback: (Boolean, String, List<AppointmentModel>) -> Unit
+    )
+
+
+}
