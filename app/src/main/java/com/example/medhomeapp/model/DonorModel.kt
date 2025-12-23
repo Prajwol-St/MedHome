7851ac334253d278778c36b1edca0a063e7ad53f
Ishan <ishanjoshi234@gmail.com>
2025-12-23 19:48:54 +0545

    updated Code

--- app/src/main/java/com/example/medhomeapp/model/DonorModel.kt
+++ app/src/main/java/com/example/medhomeapp/model/DonorModel.kt
@@ -9,6 +9,21 @@ data class DonorModel(
     val isEmergencyAvailable: Boolean = false,
     val contactNumber: String ="",
     val location: String = "",
-    val lastDonationDate: Long? = null,
+    val lastDonationDate: Long = 0L,
     val timestamp: Long =  System.currentTimeMillis()
-)
+){
+    fun toMap(): Map<String, Any?>{
+        return mapOf(
+            "id" to id,
+            "userId" to userId,
+            "userName" to userName,
+            "bloodGroup" to bloodGroup,
+            "isAvailable" to isAvailable,
+            "isEmergencyAvailable" to isEmergencyAvailable,
+            "contactNumber" to contactNumber,
+            "location" to location,
+            "lastDonationDate" to lastDonationDate,
+            "timestamp" to timestamp
+        )
+    }
+}
