2c68e861981848b25b10cd0d1a9328a754aecf9e
Ishan <ishanjoshi234@gmail.com>
2025-12-25 13:41:37 +0545

    Updated Rep

--- app/src/main/java/com/example/medhomeapp/repository/BloodDonationRepo.kt
+++ app/src/main/java/com/example/medhomeapp/repository/BloodDonationRepo.kt
@@ -1,9 +1,22 @@
 package com.example.medhomeapp.repository
 
+import com.example.medhomeapp.model.BloodRequestModel
+
 interface BloodDonationRepo {
     fun postBloodRequest(
-        bloodRequest: BloodRequest,
-        onSuccess: () -> Unit
+        bloodRequest: BloodRequestModel,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    )
+    fun getAllBloodRequests(
+        onSuccess: (List<BloodRequestModel>)-> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getBloodRequestByGroup(
+        bloodGroup: String,
+        onSuccess: (List<BloodRequestModel>) -> Unit,
+
     )
 
 }
