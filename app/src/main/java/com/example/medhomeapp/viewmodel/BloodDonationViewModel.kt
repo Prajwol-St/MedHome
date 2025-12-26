cfb96785028a50376f4c3f668c1a43c1e30f14aa
Ishan <ishanjoshi234@gmail.com>
2025-12-26 21:15:34 +0545

    Updated view model

--- app/src/main/java/com/example/medhomeapp/viewmodel/BloodDonationViewModel.kt
+++ app/src/main/java/com/example/medhomeapp/viewmodel/BloodDonationViewModel.kt
@@ -2,6 +2,7 @@ package com.example.medhomeapp.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.medhomeapp.model.BloodRequestModel
+import com.example.medhomeapp.model.DonorModel
 import com.example.medhomeapp.repository.BloodDonationRepo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -23,5 +24,11 @@ class BloodDonationViewModel(
     private val _successMessage = MutableStateFlow<String?>(null)
     val successMessage: StateFlow<String?> = _successMessage.asStateFlow()
 
+    private val _donorModel = MutableStateFlow<DonorModel?>(null)
+    val donorProfile: StateFlow<DonorModel?> = _donorModel.asStateFlow()
 
+    private val _donors = MutableStateFlow<List<DonorModel>>(emptyList())
+    val donors: StateFlow<List<DonorModel>> = _donors.asStateFlow()
+
+    fun getCurrentUserId(): String? = repository.getCurrentUserId()
 }
