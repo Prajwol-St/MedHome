fc7a3e0404edd7e9c9bfe14b6174ad812756c32b
Ishan <ishanjoshi234@gmail.com>
2025-12-27 20:40:45 +0545

    Updated code

---
+++ app/src/main/java/com/example/medhomeapp/view/JoinDonorListScreen.kt
@@ -0,0 +1,12 @@
+package com.example.medhomeapp.view
+
+import androidx.compose.runtime.Composable
+import com.example.medhomeapp.viewmodel.BloodDonationViewModel
+
+@Composable
+fun JoinDonorListScreen(
+    viewModel: BloodDonationViewModel,
+    onBackClick: () -> Unit
+){
+
+}
