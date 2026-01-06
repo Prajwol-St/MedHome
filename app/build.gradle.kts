2ea5321844df6db1595b41ef97d5d1c3205031f3
SAUGAT960 <robonile4@gmail.com>
2026-01-06 17:13:42 +0545

    made card clickable in users

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.runtime.livedata)
+    // In your app/build.gradle
+    implementation("androidx.navigation:navigation-compose:2.8.0")
 
     //  REQUIRED FOR ICONS
     implementation("androidx.compose.material:material-icons-extended")
