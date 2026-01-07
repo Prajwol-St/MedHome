2a12af1251bd1e0d07fcfba8c5a123d9aa4b2207
Prajwol-St <Prajwools272@gmail.com>
2026-01-07 14:07:04 +0545

    consultation booked

--- app/src/main/java/com/example/medhomeapp/viewmodel/DoctorViewModelFactory.kt
+++ app/src/main/java/com/example/medhomeapp/viewmodel/DoctorViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.example.medhomeapp.viewmodel
 
-class DoctorViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.medhomeapp.repository.DoctorRepoImpl
+
+class DoctorViewModelFactory(
+    private val repo: DoctorRepoImpl
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DoctorViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return DoctorViewModel(repo) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
