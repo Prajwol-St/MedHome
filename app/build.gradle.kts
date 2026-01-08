2683d057d380f87ae47b5be17ce00548765a9194
Ishan <ishanjoshi234@gmail.com>
2026-01-08 20:20:59 +0545

    Added api service codes in calories function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,13 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     //  CAMERAX
     implementation(libs.camera.camera2)
     implementation(libs.camera.lifecycle)
