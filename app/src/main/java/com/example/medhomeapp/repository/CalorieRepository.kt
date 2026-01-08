ae504a8b2f994451b9365fa4b2f4ba92394a5579
Ishan <ishanjoshi234@gmail.com>
2026-01-08 12:03:44 +0545

    Added calories crud in repository

--- app/src/main/java/com/example/medhomeapp/repository/CalorieRepository.kt
+++ app/src/main/java/com/example/medhomeapp/repository/CalorieRepository.kt
@@ -1,5 +1,6 @@
 package com.example.medhomeapp.repository
 
+import com.example.medhomeapp.model.CalorieGoalModel
 import com.example.medhomeapp.model.FoodItemModel
 
 interface CalorieRepository {
@@ -25,5 +26,61 @@ interface CalorieRepository {
         foodItemId: String,
         onSuccess: (FoodItemModel?) -> Unit,
         onError: (Exception) -> Unit
+    )
+
+    fun updateFoodItem(
+        foodItemId: String,
+        foodItem: FoodItemModel,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun deleteFoodItem(
+        foodItemId: String,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getFoodItemByMealType(
+        date: String,
+        mealType: String,
+        onSuccess: (List<FoodItemModel>) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getFoodItemByDateRange(
+        startDate: String,
+        endDate: String,
+        onSuccess: (List<FoodItemModel>) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun setCalorieGoal(
+        goal : CalorieGoalModel,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getCalorieGoal(
+        onSuccess: (CalorieGoalModel?) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun updateCalorieGoal(
+        targetCalories: Double,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun updateMacroGoals(
+        proteinGoal: Double,
+        carbsGoal: Double,
+        fatGoal: Double,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getDailySummary(
+
     )
 }
