de9cac16785dc9342bf45f62c060a9198ad77afd
Ishan <ishanjoshi234@gmail.com>
2026-01-08 11:56:58 +0545

    Updated CalorieRepository

--- app/src/main/java/com/example/medhomeapp/repository/CalorieRepository.kt
+++ app/src/main/java/com/example/medhomeapp/repository/CalorieRepository.kt
@@ -1,6 +1,29 @@
 package com.example.medhomeapp.repository
 
+import com.example.medhomeapp.model.FoodItemModel
+
 interface CalorieRepository {
 
-    
+    fun addFoodItem(
+        foodItem : FoodItemModel,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getFoodItemsByDate(
+        date: String,
+        onSuccess: (List<FoodItemModel>) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getAllFoodItems(
+        onSuccess: (List<FoodItemModel>) -> Unit,
+        onError: (Exception) -> Unit
+    )
+
+    fun getFoodItemById(
+        foodItemId: String,
+        onSuccess: (FoodItemModel?) -> Unit,
+        onError: (Exception) -> Unit
+    )
 }
