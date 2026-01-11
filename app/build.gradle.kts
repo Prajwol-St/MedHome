6c17a06a5ff80c43be50f3092c989a9cfcef9e86
Prabhash39 <prabhashsigdel10@gmail.com>
2026-01-11 15:10:33 +0545

    Updated dependencies and organize build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,61 +45,59 @@ android {
 
 dependencies {
 
-    // = CORE
+    //CORE
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
-    //  COMPOSE
+    // COMPOSE
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.runtime.livedata)
-
-    //  REQUIRED FOR ICONS
     implementation("androidx.compose.material:material-icons-extended")
 
-    //  LIFECYCLE
+    //LIFECYCLE
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
 
-    //  FIREBASE
+    // FIREBASE
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-database-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
 
-    //  GOOGLE AUTH
+    // GOOGLE AUTH
     implementation("com.google.android.gms:play-services-auth:21.4.0")
 
-    //  CAMERAX
+    //CLOUDINARY & IMAGE LOADING
+    implementation("com.cloudinary:cloudinary-android:3.0.2")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // CAMERAX
     implementation(libs.camera.camera2)
     implementation(libs.camera.lifecycle)
     implementation(libs.camera.view)
 
-    //  BARCODE
+    // BARCODE
     implementation(libs.zxing.android.embedded)
     implementation(libs.play.services.mlkit.barcode.scanning)
 
     //  UTIL
     implementation(libs.guava)
 
-    //  TESTING
+    // TESTING
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
-    //  DEBUG
+    // DEBUG
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-    // Cloudinary
-    implementation("com.cloudinary:cloudinary-android:3.0.2")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("io.coil-kt:coil-compose:2.5.0")
 }
