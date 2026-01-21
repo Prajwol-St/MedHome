4974842978a46b9d963a77a242668a06ff56b993
Ishan <ishanjoshi234@gmail.com>
2026-01-21 13:02:27 +0545

    Added Notifications Model and extension functions

---
+++ app/src/main/java/com/example/medhomeapp/utils/NotificationTypes.kt
@@ -0,0 +1,10 @@
+package com.example.medhomeapp.utils
+
+object NotificationTypes {
+    const val APPOINTMENT_24H = "appointment_24h"
+    const val APPOINTMENT_1H = "appointment_1h"
+    const val MEDICINE_REMINDER = "medicine"
+    const val BOOKING_CONFIRMATION = "booking_confirmation"
+    const val APPOINTMENT_CANCELLED = "appointment_cancelled"
+    const val APPOINTMENT_RESCHEDULED = "appointment_rescheduled"
+}
